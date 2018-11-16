1759d0acf9e8dde847520ba9467578515177e4d4
Daniel Trujillo <ganimedes@firemail.cc>
2018-11-16 16:08:13 +0100

    styles created

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'cocktails#index'
   resources :cocktails, only: [:index, :show, :new, :create] do
-    resources :doses, only: [:create]
+    resources :doses, only: [:create, :new]
   end
   resources :doses, only: :destroy
 end
